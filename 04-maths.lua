261879c5022d9766855bfc13788317e4fc6022ac
LinC3e <ivancostilla17@gmail.com>
2024-01-02 16:21:10 -0300

    ADD - Maths

---
+++ 04-maths.lua
@@ -0,0 +1,23 @@
+print(2 + 2)
+print(3 - 2)
+print(2 * 2)
+print(6 / 2)
+print(2 ^ 3)
+print(17 % 5)
+
+print("---------")
+print(math.pi)
+print(math.min(23,4,123,55,32))
+print(math.max(23,4,123,55,32))
+
+
+print("---------")
+print(math.ceil(20.3)) -- round up
+print(math.ceil(20.9))
+
+print(math.floor(20.3))
+print(math.floor(20.9))
+
+print("---------")
+print(math.random())
+print(math.random(10))
