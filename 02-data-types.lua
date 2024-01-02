1d61fb3303c888a34cdc472500ae62b2ea162da1
LinC3e <ivancostilla17@gmail.com>
2024-01-02 15:25:55 -0300

    ADD - Data Types

---
+++ 02-data-types.lua
@@ -0,0 +1,13 @@
+--[[
+    nil
+    number 1 2 3 ... -99 9.8
+    string "asd" 'asde'
+    boolean true false
+    tables
+]]
+
+local x = 2 -- Local Variable
+_G.num = 3 -- Global variable
+
+local one, two, three = "one", 2, true
+print(one,two,three)
