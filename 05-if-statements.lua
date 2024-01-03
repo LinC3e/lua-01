a226e310e33f9b6b0384577be40a5c6da55bd403
LinC3e <ivancostilla17@gmail.com>
2024-01-03 17:04:59 -0300

    ADD - If statements

---
+++ 05-if-statements.lua
@@ -0,0 +1,24 @@
+local age = 17
+local x = 4
+
+if age >= 18 then
+    print("You are an adult")
+    else
+        print("You are not an adult")
+end
+
+if x < 5 and x >= 2 then
+    print('X is less than 5')
+end
+
+--[[
+    and
+    or
+    not
+    >
+    <
+    >=
+    <=
+    ~=
+    ==
+]]
