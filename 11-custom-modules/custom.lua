f3f8f9061204631a22e7e94afe30f27211db4116
LinC3e <ivancostilla17@gmail.com>
2024-01-04 16:07:58 -0300

    ADD - Custom modules

---
+++ 11-custom-modules/custom.lua
@@ -0,0 +1,11 @@
+Mod = {
+    sum = function (x, y)
+        return x + y
+    end
+}
+
+function Mod.sayHi(name)
+    print("Hello, " .. name)
+end
+
+return Mod
---
+++ 11-custom-modules/main.lua
@@ -0,0 +1,4 @@
+local mod = require("custom")
+
+print(mod.sum(2,2))
+print(mod.sayHi("Joseph"))
